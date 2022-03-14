672dfbed033a4ae2243faef6122da0cab956652e
manmohanaeir1 <manmohanaeir001110@gmail.com>
2022-03-14 13:10:35 +0545

    pinter to object

---
+++ oop/pointerToObject/first.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class items{
+    int code, price;
+    public :
+    void getData(){
+        cout <<"Enter code and price" <<endl;
+        cin>>code>>price;
+
+    }
+    void showData(){
+        cout <<"Code " <<code <<endl;
+        cout <<"Price "<<price <<endl; 
+    }
+};
+
+int main(){
+    items *a = new items();
+    items b;
+    a->getData();
+    b.showData();
+    cout<<"Items details";
+    cout <<"First iteams "<<endl;
+    a->showData();
+    cout<< "Second iteams" <<endl;
+    b.showData();
+    return 0;
+
+
+
+
+}
