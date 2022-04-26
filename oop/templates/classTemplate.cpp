96a7dcbedc9013aacd56de19f8421c9f962f9966
ManmohanAeir <manmohanaeir001110@gmail.com>
2022-04-26 14:44:37 +0545

    classTemplate

---
+++ oop/templates/classTemplate.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+template <class T>
+class A{
+    public:
+    T n1 = 6;
+     T n2 = 7;
+void getNum()
+{
+    cout<< "Addition of two number is :" <<n1+n2 <<endl;
+   
+   
+}
+};
+
+int main()
+{
+    A<int> d;
+    d.getNum();
+    return 0;
+}
+
+
+
+
